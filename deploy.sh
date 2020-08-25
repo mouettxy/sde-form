9e093de34a7bd871095f4745a27033aa21e557a0
newfox79 <znaykaznaykovich@gmail.com>
2020-08-25 21:28:34 +0300

    fix(travis): trying to fix ftp upload

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-
-#Change FTP_ADDRESS with your FTP address. To ensure this works, visit the FTP address in your internet browser to get the right path
-find dist -type f -exec curl -u $SFTP_USER:$SFTP_PASSWORD --ftp-create-dirs -T {} ftp://api.sde.ru.com/form/{} \;
