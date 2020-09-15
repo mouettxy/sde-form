bf76519029ddbb7553cced9d2da8432777ace15f
newfox79 <znaykaznaykovich@gmail.com>
2020-09-15 17:32:32 +0300

    feat: rewrite main app to handle main view

--- src/App.vue
+++ src/App.vue
@@ -1,126 +1,255 @@
 <template lang="pug">
 v-app
-  sdeForm
+  v-app-bar(dense).elevation-0
+    v-app-bar-nav-icon(@click.stop='menu = true')
+    v-toolbar-title {{ $t("title") }}
+    v-spacer
+    v-btn.text-lowercase(text, color='primary', href='https://sde.ru.com', target='_blank') sde
+
+  v-navigation-drawer(v-model='menu', absolute, temporary).menu
+    v-list(dense).menu-links
+      v-list-item-group
+        v-list-item(href='https://api.sde.ru.com/client/cabinet.php', target='_blank')
+          v-list-item-title {{ $t("menu.clientCabinet") }}
+        v-list-item(href='https://sde.ru.com/#contacts', target='_blank')
+          v-list-item-title {{ $t("menu.contacts") }}
+        v-list-item(href='https://sde.ru.com/payment', target='_blank')
+          v-list-item-title {{ $t("menu.payment") }}
+        v-list-item(href='https://sde.ru.com/#prices', target='_blank')
+          v-list-item-title {{ $t("menu.deliveryCost") }}
+    v-card(flat, :color='isDark ? "accent" : "secondary"').menu-info
+      v-card-text
+        .menu-info__item(:class='{"white--text": isDark}') 10.00 - 23.00
+        a(href='tel:+79186619960', text, :class='{"white--text": isDark}').menu-info__item +7 (918) 661 99 60
+        a(href='tel:+9786525482', text, :class='{"white--text": isDark}').menu-info__item +7 (978) 652 54 82
+        .menu-info__item(:class='{"white--text": isDark}') © {{(new Date()).getFullYear()}} sde
+
+  template(v-if='pricesOverall')
+    v-scale-transition
+      PriceLabel
+
+  v-main
+    v-container.sde-form(fluid)
+      v-row(no-gutters)
+        v-col(cols='12', lg='5', md='6')
+          v-card(elevation='0')
+            v-tabs(
+              v-model='activeTab',
+              :background-color='defaultTabColor',
+              :color='defaultTabTextColor',
+              next-icon='mdi-arrow-right',
+              prev-icon='mdi-arrow-left',
+              show-arrows,
+              grow
+            )
+              v-tab {{ $t("makeOrder") }}
+              v-tab(v-if='isMobile') {{ $t("map") }}
+            v-tabs-items(touchless, v-model='activeTab')
+              v-tab-item
+                OrderView(:state='state', @state-change='onStateChange')
+              v-tab-item(v-if='isMobile')
+                MapBlock
+        v-col(cols='12', lg='7', md='6', v-if='!isMobile')
+          MapBlock
+
+  v-footer.px-2(padless, fixed, app, :color='defaultFooterColor')
+    span {{ new Date().getFullYear() }} - sde
+    v-spacer
+    v-btn(
+      icon,
+      :disabled='!$cookies.get("remembered-id")',
+      @click='defaultStartOrder',
+      :content='$t("homeMapMarkerTip")',
+      v-tippy
+    )
+      v-icon(:color='$cookies.get("fill-default-address") ? "success" : "dark"') mdi-home-map-marker
+    v-btn(icon, @click='changeTheme', :content='$t("invertColorsTip")', v-tippy)
+      v-icon(:color='isDark ? "success" : "dark"') mdi-invert-colors
+    v-btn.text-uppercase(text, @click='changeLocale', :content='$t("changeLocaleTip")', v-tippy) {{$i18n.locale}}
 </template>
 
-<script>
-import sdeForm from '@/components/sdeForm'
+<script lang="ts">
+import { colors, breakpoints } from '@/mixins'
+import { Component, Mixins, Watch } from 'vue-property-decorator'
+import eventBus from '@/eventBus'
 
-export default {
+import OrderView from '@/views/OrderView.vue'
+import MapBlock from '@/views/MapBlock.vue'
+import PriceLabel from '@/views/PriceLabel.vue'
+
+import { addressesModule, authModule } from '@/store'
+import { filter as lodashFilter } from 'lodash'
+import { OrderAddress } from './typings/order'
+
+@Component({
   name: 'App',
 
   components: {
-    sdeForm,
-  },
+    OrderView,
+    PriceLabel,
+    MapBlock
+  }
+})
+export default class App extends Mixins(colors, breakpoints) {
+  public menu = false
+  public state = 'filling'
+  public activeTab = 1
 
-  data: () => ({}),
+  @Watch('breakpoints')
+  onBreakpointsChange() {
+    if (!this.isMobile) {
+      this.activeTab = 0
+    } else {
+      this.activeTab = 1
+    }
+  }
 
-  methods: {
-    async generate() {
-      /* let clients = await http.get('/api/v2/client/')
-      console.log(clients) */
-      /* clients = _.filter(clients.data, e => {
-        return _.includes(a, e.CLIENT)
-      })
-      let clientsWithError = []
-      let clientsWithAliases = []
-      _.each(clients, async e => {
-        setTimeout(async () => {
-          if (e.CLIENT && !!_.trim(e.ADDR)) {
-            if (e.aliases === null) {
-              let newAlias = {
-                name: 'От нас / К нам',
-                address: e.ADDR,
-                lat: undefined,
-                lon: undefined,
-              }
+  get pricesOverall() {
+    return addressesModule.prices?.overall
+  }
 
-              try {
-                let suggestions = await getAddressSuggestions(newAlias.address)
-                let suggestion = suggestions[0]
-                if (!suggestion.data.geo_lat || !suggestion.data.geo_lon) {
-                  clientsWithError.push(e.CLIENT)
-                } else {
-                  newAlias.lat = suggestion.data.geo_lat
-                  newAlias.lon = suggestion.data.geo_lon
+  onStateChange(state: string) {
+    this.state = state
+  }
+  isRememberedUser() {
+    const cookie = this.$cookies.get('remembered-id')
+    if (cookie) {
+      const userId = parseInt(cookie)
+      if (!isNaN(userId)) {
+        return userId
+      }
+    }
+    return false
+  }
+  changeLocale() {
+    if (this.$i18n.locale === 'ru') {
+      this.$i18n.locale = 'en'
+    } else {
+      this.$i18n.locale = 'ru'
+    }
+  }
+  defaultStartOrder() {
+    if (this.$cookies.get('fill-default-address')) {
+      this.$cookies.remove('fill-default-address')
+      document.location.reload()
+    } else {
+      this.$cookies.set('fill-default-address', true)
+      document.location.reload()
+    }
+  }
+  changeTheme() {
+    if (this.isDark) {
+      this.$vuetify.theme.dark = false
+      this.$cookies.remove('dark-theme')
+    } else {
+      this.$vuetify.theme.dark = true
+      this.$cookies.set('dark-theme', true)
+    }
+  }
 
-                  let id = await http.post(`/api/v2/client/${e.CLIENT}/aliases/`, newAlias)
-                  console.log(id)
-                }
-              } catch (err) {
-                clientsWithError.push(e.CLIENT)
-              }
-            } else {
-              clientsWithAliases.push(e.CLIENT)
-            }
-          }
-        }, 1000)
-      })
-      console.log(clientsWithError, 'errors')
-      console.log(clientsWithAliases, 'aliases') */
-      /* let clientsWithError = []
-      _.each(clients, async e => {
-        if (e.CLIENT && !!_.trim(e.ADDR)) {
-          if (e.aliases === null) {
-            let newAlias = {
-              name: 'От нас / К нам',
-              address: e.ADDR,
-              lat: undefined,
-              lon: undefined,
-            }
+  async mounted() {
+    console.log(this.$cookies)
+    if (this.$cookies.get('dark-theme') === 'true') {
+      this.$vuetify.theme.dark = true
+    } else {
+      this.$vuetify.theme.dark = false
+    }
 
-            setTimeout(async () => {
-              try {
-                let suggestions = await getAddressSuggestions(newAlias.address)
-                let suggestion = suggestions[0]
-                if (!suggestion.data.geo_lat || !suggestion.data.geo_lon) {
-                  clientsWithError.push(e.CLIENT)
-                } else {
-                  newAlias.lat = suggestion.data.geo_lat
-                  newAlias.lon = suggestion.data.geo_lon
+    const isRememeredUser = this.isRememberedUser()
+    if (typeof isRememeredUser === 'number') {
+      await authModule.relog({ type: 'default', id: isRememeredUser })
+    }
 
-                  let id = await http.post(`/api/v2/client/${e.CLIENT}/aliases/`, newAlias)
-                  console.log(id)
-                }
-              } catch (err) {
-                clientsWithError.push(e.CLIENT)
-              }
-            }, 500)
-          }
-        }
-      })
+    if (this.$cookies.get('fill-default-address')) {
+      if (addressesModule.addressList.length <= 0) {
+        const defaultAddress = lodashFilter(authModule.aliases, { name: 'От нас / К нам' })[0] as OrderAddress
 
-      console.log(clientsWithError) */
-    },
-  },
+        addressesModule.add(defaultAddress)
+      }
+    }
 
-  async created() {},
+    if (this.isMobile) {
+      this.activeTab = 2
+      setTimeout(() => {
+        this.activeTab = 1
+      }, 500)
+    } else {
+      this.activeTab = 1
+    }
+
+    eventBus.$on('order-sended-saved', () => {
+      this.state = 'filling'
+    })
+    eventBus.$on('order-sended', () => {
+      this.state = 'filling'
+    })
+    eventBus.$on('order-sended-error', () => {
+      this.state = 'filling'
+    })
+    eventBus.$on('address-added', (payload: any) => {
+      if (payload.name !== 'От нас / К нам' && !payload.isAlias) {
+        this.$notification.success(`Успешное добавление адреса ${payload.address}`)
+      }
+    })
+  }
 }
 </script>
 
-<style lang="stylus">
+<style lang="sass">
+@import './assets/main'
+
 html
-  overflow hidden !important
-  height 100%
+  overflow: hidden !important
+  height: 100%
 
 body
-  overflow hidden
-  min-height 100%
-  width 100%
+  overflow: hidden
+  min-height: 100%
+  width: 100%
 
 ::-webkit-scrollbar
-  width 8px
+  width: 8px
 
 /* Track */
 ::-webkit-scrollbar-track
-  box-shadow none
-  border-radius 0
+  box-shadow: none
+  border-radius: 0
 
 /* Handle */
 ::-webkit-scrollbar-thumb
-  background #ffcc01
-  border-radius 8px
+  background: #ffcc01
+  border-radius: 8px
 
-input:-webkit-autofill, input:-webkit-autofill:hover, input:-webkit-autofill:focus, input:-webkit-autofill:active
-  background-color inherit !important
+input:-webkit-autofill
+  -webkit-box-shadow: 0 0 0 50px #fafafa inset
+  -webkit-text-fill-color: #181818
+
+
+input:-webkit-autofill:focus
+  -webkit-box-shadow: 0 0 0 50px #fafafa inset
+  -webkit-text-fill-color: #181818
+
+
+.sde-form
+  height: $height-root-sde
+  padding: 0 !important
+
+  .row
+    height: $height-root-sde
+
+.menu
+  .v-navigation-drawer__content
+    display: flex
+    justify-content: top
+    flex-direction: column
+
+  .menu-links
+    flex-grow: 4
+
+  .menu-info
+    flex-grow: 1
+    .menu-info__item
+      display: block
+      color: rgba(0, 0, 0, 0.6)
+      text-decoration: none
 </style>
