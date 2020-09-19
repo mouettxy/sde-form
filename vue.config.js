279f0ff8a91e4f631119ece58dd0e7ad36ae38f7
newfox79 <znaykaznaykovich@gmail.com>
2020-09-19 20:22:57 +0300

    feat(*): add split chunks & change fallback locale & update manifest

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,35 @@
-// config
+/* eslint-disable @typescript-eslint/camelcase */
 module.exports = {
   transpileDependencies: ['vuetify'],
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://api.sde.ru.com/form/' : '/',
-
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  pwa: {
+    manifestOptions: {
+      name: 'Заявка на доставку sde',
+      short_name: 'Доставка sde',
+      display: 'fullscreen',
+      startUrl: 'https://api.sde.ru.com/test/',
+      msTileColor: '#000000',
+      theme_color: '#C79C00',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black'
+    }
+  },
   configureWebpack: {
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all'
+          }
+        }
+      }
+    },
     module: {
       rules: [
         {
-          test: /\.(js | vue)$/,
+          test: /\.(js | ts | vue)$/,
           exclude: /node_modules/,
           loader: 'eslint-loader',
           options: {
@@ -30,7 +52,7 @@ module.exports = {
   pluginOptions: {
     i18n: {
       locale: 'ru',
-      fallbackLocale: 'ru',
+      fallbackLocale: 'en',
       localeDir: 'locales',
       enableInSFC: true
     },
