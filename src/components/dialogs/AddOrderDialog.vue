c023c734984820ee513a8851bfdfffe0f529e733
newfox79 <znaykaznaykovich@gmail.com>
2020-09-19 20:34:34 +0300

    feat(order-dialog): add save order functionality

--- src/components/dialogs/AddOrderDialog.vue
+++ src/components/dialogs/AddOrderDialog.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.add-alias-dialog
+.add-order-dialog
   v-dialog(v-model='value', max-width='50%', :fullscreen='isMobile', transition='dialog-bottom-transition')
     v-card
       v-card-title
@@ -8,27 +8,29 @@
         v-btn(icon, @click='value = false')
           v-icon mdi-close
       v-card-text
-        h4.text-error {{$t("orderField.disclaimer")}}
+        h4.error--text {{$t("orderField.disclaimer")}}
         v-form(@submit.prevent='saveOrder')
           v-text-field(
             v-model='orderName',
-            :color='color',
+            :color='defaultInputColor',
             :label='$t("orderField.label")',
             :hint='$t("orderField.hint")',
             auocomplete='chrome-off',
             prepend-inner-icon='mdi-content-save'
           )
           v-btn(@click='saveOrder', color='primary') {{$t("orderField.btn")}}
-  slot(name='button', :open='openDialog')
+  slot(name='buttons', :open='openDialog')
 </template>
 
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator'
 
 import { breakpoints, colors } from '@/mixins'
+import { addressesModule, authModule } from '@/store'
+import { includes, map } from 'lodash'
 
 @Component
-export default class AddAddressDialog extends Mixins(breakpoints, colors) {
+export default class AddOrderDialog extends Mixins(breakpoints, colors) {
   public value = false
   public orderName = ''
 
@@ -36,8 +38,39 @@ export default class AddAddressDialog extends Mixins(breakpoints, colors) {
     this.value = true
   }
 
-  saveOrder() {
-    console.log('order-saved')
+  async saveOrder() {
+    if (!this.orderName) {
+      this.$notification.error(this.$t('orderField.hint') as string)
+    }
+
+    if (
+      includes(
+        map(authModule.addresses, (e) => e.name),
+        this.orderName
+      )
+    ) {
+      this.$notification.error('Сохранённая заявка с таким названием уже существует')
+      return Promise.resolve(false)
+    }
+
+    this.$emit('save-start')
+
+    const response = await addressesModule.saveOrder(this.orderName)
+
+    switch (response.status) {
+      case 'ERROR':
+        this.$notification.error(response.message)
+        break
+      case 'ERROR-SAVED':
+        this.$notification.error(response.message)
+        break
+      case 'OK':
+        this.$notification.success(response.message)
+        break
+      case 'OK-SAVED':
+        this.$notification.success(response.message)
+        break
+    }
   }
 }
 </script>
